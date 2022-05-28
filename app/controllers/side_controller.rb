470a86d7631c00074337ebe1f9d79c1da3e135e1
Norihiro Yamada <norihiro.yamada@di-v.co.jp>
2022-05-28 15:49:02 +0900

    viewの表示

---
+++ app/controllers/side_controller.rb
@@ -0,0 +1,4 @@
+class SideController < ApplicationController
+  def index
+  end
+end
---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { Swich, Route, Link } from 'react-router-dom'
+// import styled from 'styled-components'
+// import AddTodo from './AddTodo'
+// import TodoList from './TodoList'
+// import EditTodo from './EditTodo'
+// import './App.css'
+
+function App() {
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
