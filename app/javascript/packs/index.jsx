0f1d8f0e41e4e991358a9d60e6db7893acfa7f2f
Norihiro Yamada <norihiro.yamada@di-v.co.jp>
2022-05-28 17:43:45 +0900

    トップページの作成

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(
     <BrowserRouter>
       <App/>
     </BrowserRouter>,
-    document.querySelector('#root'),
   );
 });
